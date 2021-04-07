4b590f47dc8d7f04bb5cc11edc599144f8f86f08
fariba661023 <fariba661023@gmail.com>
2021-04-07 03:59:31 -0700

    add changes

---
+++ android/app/src/linking.js
@@ -0,0 +1,16 @@
+const config ={
+    screens:{
+        StartQRCode:{
+            path:"StartQRCode"
+        },
+        QRCode:{
+            path:"QRCode"
+        }
+    },
+};
+
+const Linking={
+    prefixes:["demo://app"],
+    config
+};
+ export default Linking
